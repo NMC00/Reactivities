5743f37811b587544e101cf8eead6f40a2d610e6
NMC00 <96028518+NMC00@users.noreply.github.com>
2021-12-31 09:07:14 -0600

    End of Section 9

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from "react";
 import { Grid } from "semantic-ui-react";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { useStore } from "../../../app/stores/store";
+import ActivityFilters from "./ActivityFilters";
 import ActivityList from "./ActivityList";
 
 function ActivityDashboard(){
@@ -21,8 +22,7 @@ function ActivityDashboard(){
                 <ActivityList />
             </Grid.Column>
             <Grid.Column width='6'>
-                <h2>Activity Filters</h2>
-                <span>Coming soon...</span>
+                <ActivityFilters />
             </Grid.Column>
         </Grid>
     )
